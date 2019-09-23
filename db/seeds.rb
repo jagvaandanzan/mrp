35cd9e5dce36cc855c120482501b6fa9cce841f3
Bayaraa <ch.enkhbayar07@gmail.com>
2019-09-23 17:18:10 +0800

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminPermission.destroy_all
-AdminPermission.create!([{:id => 1, :name => 'Админ', :description => 'Админ'}])
-AdminUser.create!(:email => 'enkhamgalan.ch@icloud.com', :admin_permission_id => 1, :password => 'pass1234', :password_confirmation => 'pass1234') if Rails.env.development?
+puts '[START] load master data.'
+load(Rails.root.join('db', 'seeds', 'master.rb'))
+puts '[END] load master data.'
 
-UserPosition.destroy_all
-UserPosition.create!([{:name => 'Менежер'}, {:name => 'Нярав'}, {:name => 'Оператор'}])
-UserPermission.destroy_all
-UserPermission.create!([{:id => 1, :name => 'Админ', :description => 'Админ'}])
+########################################################
+puts '[START] load test data.'
+load(Rails.root.join('db', 'seeds', "#{Rails.env.downcase}.rb"))
+puts '[END] load test data.'
---
+++ db/seeds/master.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+AdminPermission.destroy_all
+AdminPermission.create!([{:id => 1, :name => 'Админ', :description => 'Админ'}])
+
+UserPosition.destroy_all
+UserPosition.create!([{:name => 'Менежер'}, {:name => 'Нярав'}, {:name => 'Оператор'}])
+UserPermission.destroy_all
+UserPermission.create!([{:id => 1, :name => 'Админ', :description => 'Админ'}])
+
+#AdminUser.create!(:email => 'enkhamgalan.ch@icloud.com', :admin_permission_id => 1, :password => 'oi0987', :password_confirmation => 'oi0987') if Rails.env.development?
