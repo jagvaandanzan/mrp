d07c0a556af7db52448ad2ce28757334e003770c
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-03-14 14:44:27 +0800

    coping aliexpress

--- app/controllers/api/user/travels.rb
+++ app/controllers/api/user/travels.rb
@@ -2,25 +2,24 @@ module API
   module USER
     class Travels < Grape::API
       resource :travels do
-        resource :salesman do
-          desc "POST travels/salesman"
-          params do
-            requires :signed, type: Boolean
-            optional :date, type: DateTime
-          end
-          post do
-            user = current_user
-            # if user.is_stockkeeper?
-              date = if params[:date].present?
-                       ApplicationController.helpers.local_date(params[:date])
-                     else
-                       nil #Time.now.beginning_of_day
-                     end
+        desc "POST travels"
+        params do
+          requires :signed, type: Boolean
+          optional :date, type: DateTime
+        end
+        post do
+          user = current_user
+          if user.is_stockkeeper?
+            date = if params[:date].present?
+                     ApplicationController.helpers.local_date(params[:date])
+                   else
+                     nil #Time.now.beginning_of_day
+                   end
 
-              travels = SalesmanTravel.by_signed(params[:signed], date)
-              present :travels, travels, with: API::USER::Entities::SalesmanTravels
-            # end
+            travels = SalesmanTravel.by_signed(params[:signed], date)
+            present :travels, travels, with: API::USER::Entities::SalesmanTravels
           end
+
         end
 
         route_param :id do
