8770e3a06962a1d5ac46192484c655e9db442a84
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-04-17 17:39:56 +0800

    set auto sales

--- db/migrate/20200416062942_add_tags_to_fb_comments.rb
+++ db/migrate/20200416062942_add_tags_to_fb_comments.rb
@@ -1,6 +1,5 @@
 class AddTagsToFbComments < ActiveRecord::Migration[5.2]
   def change
-    remove_column :fb_comments, :replied, :boolean
     remove_column :fb_comments, :is_hide, :boolean
     remove_column :fb_comments, :fb_comment_id, :references
     remove_column :fb_comments, :reply_id, :references
