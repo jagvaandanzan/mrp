b645bf237759d7c93b8a1e0681295df11760ae20
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-04-22 15:32:38 +0800

    noti

--- app/controllers/admin_users/facebooks_controller.rb
+++ app/controllers/admin_users/facebooks_controller.rb
@@ -39,24 +39,27 @@ class AdminUsers::FacebooksController < AdminUsers::BaseController
     unless after.nil?
       puts "#{after}"
     end
-    ApplicationController.helpers.api_send("#{ENV['FB_API']}#{ENV['FB_PAGE_ID']}/posts?access_token=#{ENV['FB_TOKEN']}&fields=id,message#{after.nil? ? '' : '&after=' + after}", 'get', nil)
+    ApplicationController.helpers.api_send("#{ENV['FB_API']}#{ENV['FB_PAGE_ID']}/posts?access_token=#{ENV['FB_TOKEN']}&fields=id,message,created_time,is_published#{after.nil? ? '' : '&after=' + after}", 'get', nil)
   end
 
   def parse_post(data)
     data.each do |json|
-      product_name = nil
-      product_code = nil
-      if json['message'].present?
-        json['message'].lines.each_with_index {|line, index|
-          if index == 0
-            product_name = line.squish
-          elsif line.start_with? "#Код:"
-            product_code = line.gsub('#Код:', '').gsub(' ', '').squish
-            break
+      if json['is_published']
+        product_name = nil
+        product_code = nil
+        if json['message'].present?
+          json['message'].lines.each_with_index {|line, index|
+            if index == 0
+              product_name = line.squish
+            elsif line.start_with? "#Код:"
+              product_code = line.gsub('#Код:', '').gsub(' ', '').squish
+              break
+            end
+          }
+          if !product_name.nil? && !product_code.nil?
+            data = DateTime.parse(json['created_time'])
+            FbPost.create(post_id: json['id'].gsub("#{ENV['FB_PAGE_ID']}_", ''), product_name: product_name, product_code: product_code, created_at: data, updated_at: data)
           end
-        }
-        if !product_name.nil? && !product_code.nil?
-          FbPost.create(post_id: json['id'].gsub("#{ENV['FB_PAGE_ID']}_", ''), product_name: product_name, product_code: product_code)
         end
       end
     end
