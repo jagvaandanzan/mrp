ac6d2b676dde8eb7b4573caefcf0923f74fe24de
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-04-27 12:54:36 +0800

    fb_post update

--- app/controllers/users/pages_controller.rb
+++ app/controllers/users/pages_controller.rb
@@ -1,14 +1,14 @@
 class Users::PagesController < Users::BaseController
 
   def panel
-    params = {email: ENV['SERVER_UID']}
-
-    response = ApplicationController.helpers.api_request('products', 'post', params)
-    Rails.logger.debug(response.body.to_s)
-    if response.code.to_i == 200
-      Rails.logger.debug(response.body.to_s)
-      data = MultiJson.load(response.body)
-    end
+    # params = {email: ENV['SERVER_UID']}
+    #
+    # response = ApplicationController.helpers.api_request('products', 'post', params)
+    # Rails.logger.debug(response.body.to_s)
+    # if response.code.to_i == 200
+    #   Rails.logger.debug(response.body.to_s)
+    #   data = MultiJson.load(response.body)
+    # end
 
   end
 end
