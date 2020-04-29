618f1dab0183c9987f3b0c6bf5a73d4c2e49bb43
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-04-29 17:18:07 +0800

    fb_post update

--- app/controllers/api/v1/notifications.rb
+++ app/controllers/api/v1/notifications.rb
@@ -170,8 +170,7 @@ def check_auto_reply(fb_post, message)
     case ac.condition
     when "phone"
       phone = message.match(/[789]\d{7}/)
-      unless phone.nil?
-        Rails.logger.debug("phone #{phone} ==>#{phone.to_s.length}")
+      if phone.nil? && phone.to_s.length == 8
         fb_comment_action = ac
       end
     when "contain"
