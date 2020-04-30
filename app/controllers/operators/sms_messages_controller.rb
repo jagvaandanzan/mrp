d32e10af37e7dafa61ff156f0cb881e008c35c0a
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-04-30 12:11:31 +0800

    fb_post update

--- app/controllers/operators/sms_messages_controller.rb
+++ app/controllers/operators/sms_messages_controller.rb
@@ -22,13 +22,28 @@ class Operators::SmsMessagesController < Operators::BaseController
   def create
     @sms_message = SmsMessage.new(sms_message_params)
 
-    if @sms_message.save
-      flash[:success] = t('alert.saved_successfully')
-      redirect_to action: :index
+    if @sms_message.valid?
+
+      response = ApplicationController.helpers.api_send("http://web2sms.skytel.mn/apiSend?token=#{ENV['SKYTEL_SMS']}&sendto=#{@sms_message.recipient}&message=#{@sms_message.full_message}", 'post')
+      Rails.logger.info("web2sms=> #{response.code.to_s} => #{response.body.to_s}")
+      if response.code.to_i == 200
+
+        json = JSON.parse(response.body)
+        if json["status"] == 1
+          @sms_message.save
+          flash[:success] = t('alert.saved_successfully')
+          redirect_to action: :index
+        else
+          @sms_message.errors.add(:recipient, ": мессеж явсангүй!")
+          render 'new'
+        end
+      end
+
     else
       logger.info(@sms_message.errors.full_messages)
       render 'new'
     end
+
   end
 
   def edit
@@ -44,4 +59,5 @@ class Operators::SmsMessagesController < Operators::BaseController
     params.require(:sms_message).permit(:recipient, :amount)
         .merge(:operator => current_operator)
   end
+
 end
--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,8 +1,6 @@
 class SmsMessage < ApplicationRecord
   belongs_to :operator
 
-  after_create :send_sms
-
   # validates :message, presence: true, length: {maximum: 160}
   validates :amount, presence: true, numericality: {greater_than: 100, only_integer: true, message: :invalid}
   validates :recipient, numericality: {greater_than_or_equal_to: 80000000, less_than_or_equal_to: 99999999, only_integer: true, message: :invalid}
@@ -22,12 +20,5 @@ class SmsMessage < ApplicationRecord
 
   def full_message
     I18n.t('sms.to_customer', amount: amount)
-  end
-
-  private
-
-  def send_sms
-    response = ApplicationController.helpers.api_send("http://web2sms.skytel.mn/apiSend?token=#{ENV['SKYTEL_SMS']}&sendto=#{recipient}&message=#{full_message}", 'post')
-    Rails.logger.info("web2sms=> #{response.code.to_s} => #{response.body.to_s}")
   end
 end
