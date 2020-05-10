0919bb37e80457a33b39208f113df3eb8bfceac0
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-05-10 12:37:14 +0800

    fb_post update

--- app/assets/javascripts/channels/fb_comment_channel.js
+++ app/assets/javascripts/channels/fb_comment_channel.js
@@ -1,12 +1,9 @@
-alert("FbCommentChannel");
 App.order = App.cable.subscriptions.create("FbCommentChannel", {
     connected: function () {
-        alert("connected")
     },
     disconnected: function () {
     },
     received: function (data) {
-        alert(data['comment']);
         var tbody = $('tbody#fb_comment_index');
         if (tbody !== undefined) {
             if (data['destroy'] !== undefined) {
