a820b32b3b6176c23941f8e79d15d04eaae72c7f
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-05-10 19:04:34 +0800

    sms add

--- db/migrate/20200510093401_create_bank_accounts.rb
+++ db/migrate/20200510093401_create_bank_accounts.rb
@@ -1,4 +1,4 @@
-class CreateBankAccounts < ActiveRecord::Migration[6.0]
+class CreateBankAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bank_accounts do |t|
       t.string :name
