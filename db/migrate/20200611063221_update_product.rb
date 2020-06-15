cb7916f24bd0e221e9f8bc1da9c49f01a2763d6b
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-06-15 10:29:56 +0800

    sms add

--- db/migrate/20200611063221_update_product.rb
+++ db/migrate/20200611063221_update_product.rb
@@ -1,7 +1,7 @@
 class UpdateProduct < ActiveRecord::Migration[5.2]
   def change
-    # add_reference :product_size_instructions, :product_feature_option, foreign_key: true, after: 'product_id'
-    # add_column :product_categories, :is_clothes, :boolean, after: 'parent_id'
+    add_reference :product_size_instructions, :product_feature_option, foreign_key: true, after: 'product_id'
+    add_column :product_categories, :is_clothes, :boolean, after: 'parent_id'
     # remove_attachment :product_feature_items, :video
     # add_attachment :products, :picture, after: 'gift_wrap'
     #
@@ -10,7 +10,7 @@ class UpdateProduct < ActiveRecord::Migration[5.2]
     # remove_column :products, :gift_wrap, :integer
     # remove_column :products, :weight, :string
 
-    remove_reference :product_specifications, :technical
-    add_reference :product_specifications, :spec_item, foreign_key: {to_table: :technical_spec_items}, after: 'product_id'
+    # remove_reference :product_specifications, :technical
+    # add_reference :product_specifications, :spec_item, foreign_key: {to_table: :technical_spec_items}, after: 'product_id'
   end
 end
