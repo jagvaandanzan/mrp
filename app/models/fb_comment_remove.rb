4f575634f4e2bc09162f07e7f2f62ab69bf5a638
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-06-18 18:41:01 +0800

    SECRET_KEY_BASE

--- app/models/fb_comment_remove.rb
+++ app/models/fb_comment_remove.rb
@@ -1,3 +1,3 @@
 class FbCommentRemove < ApplicationRecord
-  enum verb: {is_add: 0, is_hide: 1, is_remove: 2, is_reaction: 3, is_edited: 4}
+  enum verb: {is_add: 0, is_hide: 1, is_remove: 2, is_reaction: 3, is_edited: 4, user_hide: 6, user_remove: 7}
 end
