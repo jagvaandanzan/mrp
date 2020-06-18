9a78b2b2387146c71a7ce40dc04e49b8239baee6
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-06-18 20:12:50 +0800

    update category filters

--- app/controllers/users/product_categories_controller.rb
+++ app/controllers/users/product_categories_controller.rb
@@ -60,8 +60,8 @@ class Users::ProductCategoriesController < Users::BaseController
   end
 
   def category_params
-    params.require(:product_category).permit(:parent_id, :queue, :name,  :name_en, :code,
-                                             category_filter_groups_attributes: [:id, :name, :name_en, :_destroy,
-                                                                                 category_filters_attributes: [:id, :name, :name_en, :img, :_destroy]])
+    params.require(:product_category).permit(:parent_id, :queue, :name, :name_en, :code)
+    # category_filter_groups_attributes: [:id, :name, :name_en, :_destroy,
+    #                                     category_filters_attributes: [:id, :name, :name_en, :img, :_destroy]])
   end
 end
