6caf6cf8e35c5e3c4edc7bbbe66cbfa702f1b331
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-06-30 14:50:57 +0800

    fix api

--- spec/features/bank_transactions_spec.rb
+++ spec/features/bank_transactions_spec.rb
@@ -12,10 +12,10 @@ describe "bank transaction check", type: :feature do
       fill_in 'txtPassword', with: ENV['KHAN_PASSWORD']
       click_link "btnLogin"
     end
-
-    # page.has_selector?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl')
     puts "click 1: " + Time.now.to_s
-    page.has_css?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl', wait: 120)
+    sleep 1.minute
+    page.has_selector?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl')
+    # page.has_css?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl', wait: 120)
     puts "Logged: " + Time.now.to_s
     visit('/pageMain?content=ucAcnt_Statement')
     # fill_in 'ctl00$cphMain$ctl00$numBegDate', with: '2020.01.09'
