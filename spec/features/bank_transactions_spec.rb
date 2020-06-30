308f8e7ddcc0acbe476ba2c48ac85e2a8e9d670e
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-06-30 15:19:11 +0800

    fix api

--- spec/features/bank_transactions_spec.rb
+++ spec/features/bank_transactions_spec.rb
@@ -13,7 +13,7 @@ describe "bank transaction check", type: :feature do
       click_link "btnLogin"
     end
     puts "click 1: " + Time.now.to_s
-    sleep 1.minute
+    sleep 6.minute
     page.has_css?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl', wait: 0)
     page.has_selector?('a#cphMain_ctl00_ucAcnt_Menu1_acntJrnl')
     puts "Logged: " + Time.now.to_s
