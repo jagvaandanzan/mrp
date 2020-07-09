41c40b6f41f3c1b62dae9818d925a4e3bcc0a197
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-07-09 17:21:22 +0800

    income

--- db/migrate/20200704114024_create_shipping_ub_items.rb
+++ db/migrate/20200704114024_create_shipping_ub_items.rb
@@ -1,21 +1,21 @@
 class CreateShippingUbItems < ActiveRecord::Migration[5.2]
   def change
-    create_table :shipping_ub_items do |t|
-      t.references :shipping_ub, null: false, foreign_key: true
-      t.references :product_supply_feature, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
-      t.references :shipping_er_item, null: false, foreign_key: true
-      t.integer :loaded
-      t.integer :cargo
-      t.integer :s_type
-      t.float :cost
-
-      t.timestamps
-    end
-    add_column :product_incomes, :cargo_price, :integer, after: 'note'
-    remove_column :product_incomes, :note, :string
-
-    add_reference :product_income_items, :shipping_ub_item, foreign_key: true, after: 'product_income_id'
+    # create_table :shipping_ub_items do |t|
+    #   t.references :shipping_ub, null: false, foreign_key: true
+    #   t.references :product_supply_feature, null: false, foreign_key: true
+    #   t.references :product, null: false, foreign_key: true
+    #   t.references :shipping_er_item, null: false, foreign_key: true
+    #   t.integer :loaded
+    #   t.integer :cargo
+    #   t.integer :s_type
+    #   t.float :cost
+    #
+    #   t.timestamps
+    # end
+    # add_column :product_incomes, :cargo_price, :integer, after: 'note'
+    # remove_column :product_incomes, :note, :string
+    #
+    # add_reference :product_income_items, :shipping_ub_item, foreign_key: true, after: 'product_income_id'
     add_reference :product_income_items, :supply_feature, foreign_key: {to_table: :product_supply_features}, after: 'shipping_ub_id'
     remove_reference :product_income_items, :supply_order_item
     remove_reference :product_income_items, :product_supplier
@@ -30,7 +30,6 @@ class CreateShippingUbItems < ActiveRecord::Migration[5.2]
     remove_column :product_income_items, :date, :datetime
     remove_column :product_income_items, :note, :string
     remove_column :product_income_items, :urgent_type, :integer
-    add_reference :product_incomes, :shipping_ub, foreign_key: true, after: ' id '
     remove_column :product_incomes, :sum_price, :float
 
   end
