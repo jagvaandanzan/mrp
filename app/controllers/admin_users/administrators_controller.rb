b1b53c332b90db2355dc453e2ddc35fdfd626efa
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-07-15 15:36:57 +0800

    update color

--- app/controllers/admin_users/administrators_controller.rb
+++ app/controllers/admin_users/administrators_controller.rb
@@ -4,13 +4,13 @@ class AdminUsers::AdministratorsController < AdminUsers::BaseController
 
   def index
     @admin_users = AdminUser.all.page(params[:page])
-    cat_colors = CategoryFilterGroup.search_name("color").map(&:id).to_a
-    colors = CategoryFilter.by_group(cat_colors)
-    colors.each do |c|
-      ProductFeatureOption.create(name: c, name_en: c, product_feature_id: 1, group_id: 1)
-    end
-    groups = CategoryFilterGroup.qe_name('color')
-    groups.destroy_all
+    # cat_colors = CategoryFilterGroup.search_name("color").map(&:id).to_a
+    # colors = CategoryFilter.by_group(cat_colors)
+    # colors.each do |c|
+    #   ProductFeatureOption.create(name: c, name_en: c, product_feature_id: 1, group_id: 1)
+    # end
+    # groups = CategoryFilterGroup.qe_name('color')
+    # groups.destroy_all
   end
 
   def new
