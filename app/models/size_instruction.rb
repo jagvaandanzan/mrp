24d13cb83e0d729af19b9f9e1995f484a931f1f1
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-07-23 15:27:51 +0800

    sync

--- app/models/size_instruction.rb
+++ app/models/size_instruction.rb
@@ -1,4 +1,9 @@
 class SizeInstruction < ApplicationRecord
+  after_create -> {sync_web('post')}
+  after_update -> {sync_web('update')}, unless: Proc.new {self.method_type == "sync"}
+  after_destroy -> {sync_web('delete')}
+  attr_accessor :method_type
+
   validates :instruction, presence: true, length: {maximum: 255}
 
   scope :order_queue, ->() {
@@ -11,5 +16,22 @@ class SizeInstruction < ApplicationRecord
     items = items.where('instruction LIKE :value', value: "%#{search_instruction}%") if search_instruction.present?
     items
   }
+
+  private
+
+  def sync_web(method)
+    self.method_type = method
+    url = "resources/size_instruction"
+
+    if method == 'delete'
+      params = nil
+      url += "/" + id.to_s
+    else
+
+      params = self.to_json(only: [:id, :queue, :instruction], :methods => [:method_type])
+    end
+
+    ApplicationController.helpers.api_request(url, method, params)
+  end
 
 end
