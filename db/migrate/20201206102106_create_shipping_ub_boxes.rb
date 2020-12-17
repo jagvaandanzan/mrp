4b145fb31f029563f9ff45ffd8716af28c452340
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2020-12-17 22:15:52 +0800

    update income

--- db/migrate/20201206102106_create_shipping_ub_boxes.rb
+++ db/migrate/20201206102106_create_shipping_ub_boxes.rb
@@ -7,7 +7,7 @@ class CreateShippingUbBoxes < ActiveRecord::Migration[5.2]
     # end
     #
     # add_column :shipping_ubs, :s_type, :integer, after: 'date'
-    drop_table :shipping_ub_items
-    drop_table :shipping_er_items
+    # drop_table :shipping_ub_items
+    # drop_table :shipping_er_items
   end
 end
