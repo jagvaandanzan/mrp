1be2dba715a00cd4e4454584180df7f38a1e3a07
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-02-12 00:10:09 +0800

    itoms back

--- db/migrate/20210116114047_create_sale_taxes.rb
+++ db/migrate/20210116114047_create_sale_taxes.rb
@@ -1,8 +1,8 @@
 class CreateSaleTaxes < ActiveRecord::Migration[5.2]
   def change
-    add_column :product_feature_items, :p_6_8_p, :integer, after: 'p_6_8'
-    add_column :product_feature_items, :p_9_p, :integer, after: 'p_9_'
-    add_reference :product_location_balances, :product_feature_item, after:'feature_item_id'
-    # remove_reference :product_location_balances,:feature_item
+    # add_column :product_feature_items, :p_6_8_p, :integer, after: 'p_6_8'
+    # add_column :product_feature_items, :p_9_p, :integer, after: 'p_9_'
+    # add_reference :product_location_balances, :product_feature_item, after:'feature_item_id'
+    remove_reference :product_location_balances,:feature_item
   end
 end
