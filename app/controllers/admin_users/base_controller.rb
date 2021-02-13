111462caaede56032c9b5c979ddc42a0ba8e9266
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-02-13 19:17:37 +0800

    itoms back

--- app/controllers/admin_users/base_controller.rb
+++ app/controllers/admin_users/base_controller.rb
@@ -2,11 +2,16 @@ class AdminUsers::BaseController < ApplicationController
   layout 'admin'
 
   before_action do
+    set_locale
     authenticate_admin_user!
     redirect_to admin_passwords_edit_password_path if current_admin_user.password_is_reset
   end
 
   def routing_error
     redirect_to admin_users_root_path
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || :mn
   end
 end
