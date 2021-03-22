9f6f321adf8eddac5e449361732d864bf42d3d34
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-03-22 19:05:11 +0800

    буцаалт солилт

--- db/migrate/20210309115319_add_lang_to_logistics.rb
+++ db/migrate/20210309115319_add_lang_to_logistics.rb
@@ -1,14 +1,10 @@
 class AddLangToLogistics < ActiveRecord::Migration[5.2]
   def change
-    # add_reference :product_sales, :parent, foreign_key: {to_table: :product_sales}, after: 'sale_call_id'
-    # add_reference :product_sale_items, :parent, foreign_key: {to_table: :product_sale_items}, after: 'back_quantity'
-    #
-    # add_column :product_sales, :back_money, :integer, after: 'parent_id'
-    # add_column :product_sale_items, :bought_price, :float, after: 'bought_quantity'
-    # drop_table :product_sale_exchanges
+    add_reference :product_sales, :parent, foreign_key: {to_table: :product_sales}, after: 'sale_call_id'
+    add_reference :product_sale_items, :parent, foreign_key: {to_table: :product_sale_items}, after: 'back_quantity'
 
-    add_column :salesmen, :distribution, :float, after: 'pin_code'
-    add_column :salesmen, :price, :integer, after: 'distribution'
-    add_column :salesmen, :price_at, :datetime, after: 'price'
+    add_column :product_sales, :back_money, :integer, after: 'parent_id'
+    add_column :product_sale_items, :bought_price, :float, after: 'bought_quantity'
+    drop_table :product_sale_exchanges
   end
 end
