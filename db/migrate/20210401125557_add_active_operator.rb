7130499873e95f747ffc5b52638471bb002fbde4
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-04-02 19:34:56 +0800

    Орлого авах засварууд

--- db/migrate/20210401125557_add_active_operator.rb
+++ db/migrate/20210401125557_add_active_operator.rb
@@ -1,4 +1,4 @@
-class AddActiveOperator < ActiveRecord::Migration[6.0]
+class AddActiveOperator < ActiveRecord::Migration[5.2]
   def change
     add_reference :product_sale_calls, :active_opr, foreign_key: {to_table: :operators}, after: 'operator_id'
     remove_column :product_income_products, :calculated
