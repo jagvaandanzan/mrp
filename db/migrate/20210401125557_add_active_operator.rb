ffb52e6d2251d73ebcae71fcd56f42c5973e23c9
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-04-02 22:28:20 +0800

    Бараа буцаах хүсэлт

--- db/migrate/20210401125557_add_active_operator.rb
+++ db/migrate/20210401125557_add_active_operator.rb
@@ -1,9 +1,9 @@
 class AddActiveOperator < ActiveRecord::Migration[5.2]
   def change
-    # add_reference :product_sale_calls, :active_opr, foreign_key: {to_table: :operators}, after: 'operator_id'
-    # remove_column :product_income_products, :calculated
-    # remove_column :product_income_products, :cost_ub
-    # add_column :product_incomes, :cargo_per, :float, after: 'cargo_price'
+    add_reference :product_sale_calls, :active_opr, foreign_key: {to_table: :operators}, after: 'operator_id'
+    remove_column :product_income_products, :calculated
+    remove_column :product_income_products, :cost_ub
+    add_column :product_incomes, :cargo_per, :float, after: 'cargo_price'
     add_reference :shipping_er_products, :supply_order, foreign_key: {to_table: :product_supply_orders}, after: 'id'
     add_reference :shipping_ub_products, :supply_order, foreign_key: {to_table: :product_supply_orders}, after: 'id'
   end
