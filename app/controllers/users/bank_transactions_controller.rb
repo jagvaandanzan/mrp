748bc8bb24aa26c01e612ce8ed573782ad839654
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-04-05 18:14:55 +0800

    банкны хуулга

--- app/controllers/users/bank_transactions_controller.rb
+++ app/controllers/users/bank_transactions_controller.rb
@@ -67,7 +67,11 @@ class Users::BankTransactionsController < Users::BaseController
     @bank_transaction.is_manual = true
     if @bank_transaction.save
       flash[:success] = t('alert.info_updated')
-      redirect_to action: :index
+      if @bank_transaction.dealing_account_id.present? && !@bank_transaction.is_logistic
+        redirect_to users_bank_transaction_path(@bank_transaction, ask_print: 1)
+      else
+        redirect_to action: :index
+      end
     else
       render 'edit'
     end
