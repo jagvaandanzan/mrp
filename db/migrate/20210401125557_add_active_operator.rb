5ea66b4cd8c37040a77002475d922440c4f60f47
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-04-13 20:28:53 +0800

    web sync

--- db/migrate/20210401125557_add_active_operator.rb
+++ db/migrate/20210401125557_add_active_operator.rb
@@ -6,9 +6,9 @@ class AddActiveOperator < ActiveRecord::Migration[5.2]
     # add_column :product_incomes, :cargo_per, :float, after: 'cargo_price'
     # add_reference :shipping_er_products, :supply_order, foreign_key: {to_table: :product_supply_orders}, after: 'id'
     # add_reference :shipping_ub_products, :supply_order, foreign_key: {to_table: :product_supply_orders}, after: 'id'
-    # add_column :locations, :approved, :boolean, default: true, after: 'country'
-    # add_column :locations, :web_location_id, :bigint,  after: 'approved'
-    # add_reference :locations, :loc_district, foreign_key: true, after: 'operator_id'
+    add_column :locations, :approved, :boolean, default: true, after: 'country'
+    add_column :locations, :web_location_id, :bigint,  after: 'approved'
+    add_reference :locations, :loc_district, foreign_key: true, after: 'operator_id'
     add_column :product_sales, :cart_id, :bigint, after: 'salesman_travel_id'
   end
 end
