f4b160d266c12af37565096e1c0f1040adfa28dc
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-04-29 19:01:35 +0800

    sync bonus

--- app/models/bonus_balance.rb
+++ app/models/bonus_balance.rb
@@ -3,7 +3,7 @@ class BonusBalance < ApplicationRecord
   belongs_to :product_sale, optional: true
   belongs_to :product_sale_item, optional: true
 
-  # after_save -> {sync_web}
+  after_save -> {sync_web}
 
   scope :balance_sum, -> (bonu_id) {
     items = where(bonu_id: bonu_id)
@@ -21,9 +21,9 @@ class BonusBalance < ApplicationRecord
     bal = bonu.balance_sum
     bonu.update_column(:balance, bal)
 
-    url = "sales/bonus"
-    cart_id = product_sale.present? && product_sale.cart_id.present? ? product_sale.cart_id : 0
-    params = {phones: bonu.bonus_phones.map(&:phone).to_a, balance: bonus, bonus: bal, cart_id: cart_id}.to_json
-    ApplicationController.helpers.api_request(url, 'patch', params)
+    # url = "sales/bonus"
+    # cart_id = product_sale.present? && product_sale.cart_id.present? ? product_sale.cart_id : 0
+    # params = {phones: bonu.bonus_phones.map(&:phone).to_a, balance: bonus, bonus: bal, cart_id: cart_id}.to_json
+    # ApplicationController.helpers.api_request(url, 'patch', params)
   end
 end
