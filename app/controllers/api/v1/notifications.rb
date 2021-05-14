e412252801b843ffb88f0c0d222614c780f5aed3
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-05-14 16:21:39 +0800

    зардал тооцох алда засав

--- app/controllers/api/v1/notifications.rb
+++ app/controllers/api/v1/notifications.rb
@@ -129,8 +129,8 @@ module API
             # Rails.logger.info("itoms_balance")
             params['item_serials'].each {|ser|
               item = ProductFeatureItem.find_by_barcode(ser['serial_barcode'])
-              item.update_column(:price, ser['serial_price'].to_i)
               if item.present?
+                item.update_column(:price, ser['serial_price'].to_i)
                 product_id = item.product_id if product_id.nil?
                 items << {id: item.id, balance: ser['c2_qty'], price: ser['serial_price'].to_i}
               end
