44e91b7c1b3aed4d03be9105a44f19b07f7607a4
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-09-09 10:26:03 +0800

    засварууд

--- app/models/product_sale_item.rb
+++ app/models/product_sale_item.rb
@@ -180,7 +180,7 @@ class ProductSaleItem < ApplicationRecord
   end
 
   def set_remainder
-    self.remainder = if feature_item.balance.present?
+    self.remainder = if feature_item.present? && feature_item.balance.present?
                        feature_item.balance + (quantity_was.presence || 0) if product_id.present? && feature_item_id.present?
                      else
                        0
