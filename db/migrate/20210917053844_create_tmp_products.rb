270753b2920bcbf97d4676acfe00640ad1225a76
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-09-18 21:35:28 +0800

    report cash

--- db/migrate/20210917053844_create_tmp_products.rb
+++ db/migrate/20210917053844_create_tmp_products.rb
@@ -1,15 +1,16 @@
 class CreateTmpProducts < ActiveRecord::Migration[5.2]
   def change
-    create_table :tmp_products do |t|
-      t.string :name
-      t.string :feature
-      t.string :barcode
-      t.integer :balance
-      t.integer :price
-      t.integer :serial_id
-      t.string :location
-
-      t.timestamps
-    end
+    # create_table :tmp_products do |t|
+    #   t.string :name
+    #   t.string :feature
+    #   t.string :barcode
+    #   t.integer :balance
+    #   t.integer :price
+    #   t.integer :serial_id
+    #   t.string :location
+    #
+    #   t.timestamps
+    # end
+    add_column :product_feature_items, :init_bal, :integer, :after => "balance"
   end
 end
