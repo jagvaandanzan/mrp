03c3cad7085f7bf17f3a94a0fd7ff79d59983f61
Enkh-Amgalan <enkhamgalan.ch@icloud.com>
2021-09-21 14:55:28 +0800

    Няраваас устгах

--- db/seeds/development.rb
+++
@@ -1,7 +0,0 @@
-ProductBalance.joins(:product_sale)
-    .where("product_balances.created_at < '2021-09-20 00:00:00'")
-    .where("product_sale_items.back_quantity IS NULL AND product_sale_items.bought_quantity IS NULL AND")
-    .each do |bal|
-    puts "#{bal.id} ==> #{bal.quantity} ==> #{bal.sale_item_id} ==> #{bal.feature_item_id}"
-
-end
